1f458bccb16feb1d4e11126e38d543f5c561eed7
jonnylin13 <jonny@sqrx.com>
2025-08-21 14:34:01 -0700

    Updated

--- scripts/cp-symlinks.sh
+++ scripts/cp-symlinks.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+echo "Copying symlinks from $SCRIPT_DIR/../symlinks to $SCRIPT_DIR/../"
 
 for file in "$SCRIPT_DIR"/../symlinks/*; do
   if [ -L "$file" ]; then
     cp -L "$file" "$SCRIPT_DIR"/../
   fi
 done
+
+echo "Symlinks copied successfully."
